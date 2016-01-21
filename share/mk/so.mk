5b0a195376953361b7dc9551befda97d5d32b440
Kate F <kate@elide.org>
2016-01-21 19:03:54 +0000

    Don't clean other people's things.

--- share/mk/so.mk
+++ share/mk/so.mk
@@ -7,7 +7,6 @@ LDSFLAGS ?= -shared
 
 lib::    ${BUILD}/lib/${lib}.so
 CLEAN += ${BUILD}/lib/${lib}.so
-CLEAN += ${BUILD}/lib/${lib}.opic
 
 ${BUILD}/lib/${lib}.so: ${BUILD}/lib/${lib}.opic
 	${LD} -o $@ ${LDSFLAGS} ${.ALLSRC:M*.opic}
