b9bb7baf517371c2db9f75b85c60645465158acf
Kate F <kate@elide.org>
2016-08-01 22:57:42 +0000

    Tentative support for linking on Darwin.

--- share/mk/so.mk
+++ share/mk/so.mk
@@ -1,17 +1,30 @@
 
 LD ?= ld
+UNAME ?= uname
+UNAME_SYSTEM != ${UNAME} -s
+SYSTEM ?= ${UNAME_SYSTEM}
 
+.if ${SYSTEM} == Darwin
+LIBEXT ?= dylib
+.else
+LIBEXT ?= so
+.endif
+
+.if ${SYSTEM} == Darwin
+LDSFLAGS ?= -dynamic -flat_namespace -undefined dynamic_lookup
+.else
 LDSFLAGS ?= -shared
+.endif
 
 .for lib in ${LIB}
 
-lib::    ${BUILD}/lib/${lib}.so
-CLEAN += ${BUILD}/lib/${lib}.so
+lib::    ${BUILD}/lib/${lib}.${LIBEXT}
+CLEAN += ${BUILD}/lib/${lib}.${LIBEXT}
 
-${BUILD}/lib/${lib}.so: ${BUILD}/lib/${lib}.opic
+${BUILD}/lib/${lib}.${LIBEXT}: ${BUILD}/lib/${lib}.opic
 	${LD} -o $@ ${LDSFLAGS} ${.ALLSRC:M*.opic}
 
-STAGE_BUILD += lib/${lib}.so
+STAGE_BUILD += lib/${lib}.${LIBEXT}
 
 .endfor
 
