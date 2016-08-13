c2af7b34407d939d4c5b8e10c0cb31135a5fddbb
Kate F <kate@elide.org>
2016-08-13 01:02:03 +0000

    Stray object file to clean.

--- share/mk/ar.mk
+++ share/mk/ar.mk
@@ -8,6 +8,7 @@ ARFLAGS  ?= cr
 
 lib::    ${BUILD}/lib/${lib}.a
 CLEAN += ${BUILD}/lib/${lib}.a
+CLEAN += ${BUILD}/lib/${lib}.o
 
 ${BUILD}/lib/${lib}.a: ${BUILD}/lib/${lib}.o
 	${AR} ${ARFLAGS}${ARFLAGS_${lib}} $@ ${.ALLSRC:M*.o}
