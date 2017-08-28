630694b827d489425ede498d4a21f0306c39eee8
Kate F <kate@elide.org>
2017-08-28 12:17:22 +0200

    Avoid recreating outdated directories.

--- share/mk/mkdir.mk
+++ share/mk/mkdir.mk
@@ -7,7 +7,21 @@
 
 MKDIR ?= mkdir
 
-mkdir:: ${DIR}
+mkdir::
+
+# Double-colon targets are always considered out of date,
+# so even though these directories are up to date, they
+# would always be recreated. Note there's no need to
+# re-create an out of date directory anyway.
+#
+# So we avoid that situation here by only depending on
+# directories which do not currently exist.
+
+.for dir in ${BUILD} ${DIR}
+.if !exists(${dir})
+mkdir:: ${dir}
+.endif
+.endfor
 
 ${BUILD}:
 	${MKDIR} -p ${BUILD}
